472eaecd0adf031d5400d701997f400ede593986
udonya <udonya.dip.jp@gmail.com>
2013-05-11 12:31:45 +0900

    add base code.

--- src/main/java/com/udonya/signfix/SignFix.java
+++ src/main/java/com/udonya/signfix/SignFix.java
@@ -4,4 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class SignFix extends JavaPlugin {
 
+    @Override
+    public void onDisable() {
+        // TODO 自動生成されたメソッド・スタブ
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        // TODO 自動生成されたメソッド・スタブ
+        super.onEnable();
+    }
+
 }
